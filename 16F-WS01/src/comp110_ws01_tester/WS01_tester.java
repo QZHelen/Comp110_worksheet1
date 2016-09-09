e19217bff7216de6a137fe3ca8c0c8b45ce9d48e
QZHelen <zihe@live.unc.edu>
2016-09-09 17:03:12 -0400

    modified testing class, making field values more random

--- 16F-WS01/src/comp110_ws01_tester/WS01_tester.java
+++ 16F-WS01/src/comp110_ws01_tester/WS01_tester.java
@@ -2,38 +2,51 @@ package comp110_ws01_tester;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import comp110_ws01.Phone;
 
 public class WS01_tester {
 
-	Phone p = new Phone("Samsung", 50, 110);
-	
-	
+	Phone p = new Phone("Samsung", 51, 111);
+			
 	@Test
 	public void testCharge() {
-	
-		p.charge(10);
-		assertEquals("Charge method incorrect.",p.getPower(), 60);
-		p.charge(60);
-		assertEquals("Charge method incorrect.", p.getPower(), 100);
+		int randomNum = (int) (Math.random()*10);
+		int initial = p.getPower();
+		p.charge(randomNum);
+		assertEquals("Charge method incorrect.",initial + randomNum,p.getPower());
+		p.charge(randomNum + 60);
+		assertEquals("Charge method incorrect.", 100, p.getPower());
 		
 	}
 	
 	@Test
-	public void testUnlockPhone() {
-		assertFalse("Unlock Phone Method incorrect.", p.unlockPhone(123));
-		assertTrue("Unlock Phone Method incorrect.", p.unlockPhone(110));
-		assertEquals("Charge method incorrect.", p.getPower(), 49);
+	public void testDecreasingPower(){
+		int randomNum = (int) (Math.random() * 5);
+		p.decreasePower(randomNum);
+		assertEquals("Decreasing Power method incorrect", 51 - randomNum, p.getPower());
+		p.decreasePower(80);
+		assertEquals("Decreasing Power method incorrect", 0, p.getPower());
 	}
 	
 	@Test
-	public void resetPassword(){
+	public void testUnlockPhone() {
+		int randomWrongPw = (int) (Math.random() * 110);
+		assertFalse("Unlock Phone method incorrect.", p.unlockPhone(randomWrongPw));
+		assertTrue("Unlock Phone method incorrect.", p.unlockPhone(111));
+		assertEquals("Unlock Phone method incorrect.", p.getPower(), 50);
+		p.decreasePower(100);
+		assertFalse("Unlock Phone method incorrect.", p.unlockPhone(111));
+	}
+	
+	@Test
+	public void testResetPassword(){
 		int number = (int) (Math.random() * 100);
 		p.resetPassword(number);
 		assertEquals("Reset Password method incorrect.", p.getPassword(), number);
-		assertEquals("Charge method incorrect.", p.getPower(), 48);
+		assertEquals("Reset Password method incorrect.", p.getPower(), 49);
 	}
 
 }
